e2a7c47be83a30026eb4534520eb49cfef3c4ebd
kolkri <kristiinakolu@gmail.com>
2022-11-07 11:27:00 +0200

    1.07

--- Log output/index.js
+++ Log output/index.js
@@ -1,10 +1,25 @@
-const randomHash = Math.random().toString(36).substring(2,30)
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-const getHashNow = () => {
+
+
+// const getHashNow = () => {
   
-    console.log(new Date(), randomHash)
+//     console.log(new Date(), randomHash)
   
-    setTimeout(getHashNow, 5000)
-}
+//     setTimeout(getHashNow, 5000)
+// }
   
-getHashNow()
+// getHashNow()
+
+app.get('/', (req, res) => {
+    const randomHash = Math.random().toString(36).substring(2,30)
+    const timestamp =  new Date()
+    res.send(`<h1>Timestamp: ${timestamp}</h1>
+            <h2>Random hash: ${randomHash}</h2>`)
+})
+
+app.listen(port, () => {
+    console.log(`Server started in port ${port}`)
+  })
